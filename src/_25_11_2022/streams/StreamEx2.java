a31cd1c72e5a88a063d4bd17a50402c5f6566aa0
shonmikl <mikhailegorov676@gmail.com>
2022-11-25 14:51:11 +0300

    flatMap

--- src/_25_11_2022/streams/StreamEx2.java
+++ src/_25_11_2022/streams/StreamEx2.java
@@ -13,7 +13,7 @@ public class StreamEx2 {
     public static void main(String[] args) {
         Student s1 = new Student("Sergey", 'm', 22, 3, 8.3);
         Student s2 = new Student("Nik", 'm', 28, 2, 7.4);
-        Student s3 = new Student("Elena", 'w', 32, 1, 9.0);
+        Student s3 = new Student("Elena", 'w', 32, 2, 9.0);
         Student s4 = new Student("Petr", 'm', 30, 4, 6.0);
         Student s5 = new Student("Marya", 'w', 21, 5, 7.7);
 
@@ -50,10 +50,10 @@ public class StreamEx2 {
 //                        .stream())
 //                        .forEach(s -> System.out.println(s.getName()));
 
-        Map<Boolean, List<Student>> listMap =
+        Map<Integer, List<Student>> listMap =
                 studentList.stream()
-                        .collect(Collectors.partitioningBy(el -> el.getAvgGrade() > 6.5));
-        for (Map.Entry<Boolean, List<Student>> entry : listMap.entrySet()) {
+                        .collect(Collectors.groupingBy(Student::getCourse));
+        for (Map.Entry<Integer, List<Student>> entry : listMap.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
 
